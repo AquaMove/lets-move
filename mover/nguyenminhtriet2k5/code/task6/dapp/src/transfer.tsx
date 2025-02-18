215d40eefa4eb3385ef24c5341796b357667fa4b
nguyenminhtriet2k5 <yuvrajruihui@hotmail.com>
2025-02-18 13:51:41 +0700

    Complete task 1 2 3 4 5 6

---
+++ mover/nguyenminhtriet2k5/code/task6/dapp/src/transfer.tsx
@@ -0,0 +1,33 @@
+import { useSignAndExecuteTransaction, useCurrentAccount } from '@mysten/dapp-kit';
+import { Transaction } from '@mysten/sui/transactions';
+import { useState } from 'react';
+
+export function SendSui() {
+    const { mutateAsync: signAndExecuteTransactionBlock } = useSignAndExecuteTransaction();
+    const [Digest, SetDigest] = useState('');
+
+    function sendMessage() {
+        const txb = new Transaction();
+
+        const coin = txb.splitCoins(txb.gas, [100]);
+        txb.transferObjects([coin], '0x1a8e297da1cc25427d56c84eece1792e042822291d018d16e3b2bb84a5bdfebb');
+
+
+
+        signAndExecuteTransactionBlock({
+            transaction: txb,
+        }).then(async (result) => {
+            alert('Sui sent successfully');
+            SetDigest(result.digest);
+        });
+    }
+
+    return (
+        <div>{
+            <div>
+                <button onClick={() => sendMessage()}>Send me Sui!</button>
+                <div>Digest: {Digest}</div>
+            </div>
+        }</div>
+    );
+}
